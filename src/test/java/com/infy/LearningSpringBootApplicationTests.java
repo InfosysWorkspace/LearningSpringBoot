a507f77e02d8df39fcad90e92d85b154dc84627b
BsoftMaewada <maewada@live.com>
2022-11-10 14:39:12 -0500

    testing using jUnit and mockito

--- src/test/java/com/infy/LearningSpringBootApplicationTests.java
+++ src/test/java/com/infy/LearningSpringBootApplicationTests.java
@@ -2,23 +2,34 @@ package com.infy;
 
 import com.infy.dto.CustomerLoginDTO;
 import com.infy.exception.InfyBankException;
+import com.infy.repository.CustomerLoginRepository;
 import com.infy.service.CustomerLoginService;
+import com.infy.service.CustomerLoginServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LearningSpringBootApplicationTests {
 
-	@Autowired
-	CustomerLoginService customerLoginService;
+	@Mock
+	CustomerLoginRepository customerLoginRepository;
+
+	@InjectMocks
+	CustomerLoginService customerLoginService = new CustomerLoginServiceImpl();
 
 	@Test
 	public void authenticateCustomerTestValidCredentials() throws InfyBankException{
 		CustomerLoginDTO customer = new CustomerLoginDTO();
 		customer.setLoginName("aisha");
 		customer.setPassword("aisha123");
+
+		Mockito.when(customerLoginRepository.getCustomerLoginByLoginName("aisha")).thenReturn(customer);
+
 		String actual = customerLoginService.authenticateCustomer(customer);
 		Assertions.assertEquals("SUCCESS", actual);
 	}
@@ -28,6 +39,13 @@ class LearningSpringBootApplicationTests {
 		CustomerLoginDTO customer = new CustomerLoginDTO();
 		customer.setLoginName("aisha");
 		customer.setPassword("aisha12");
+
+		CustomerLoginDTO fromRepository = new CustomerLoginDTO();
+		fromRepository.setLoginName("aisha");
+		fromRepository.setLoginName("aisha12");
+
+		Mockito.when(customerLoginRepository.getCustomerLoginByLoginName("aisha")).thenReturn(fromRepository);
+
 		InfyBankException exception=Assertions.assertThrows(InfyBankException.class, () ->
 				customerLoginService.authenticateCustomer(customer));
 		Assertions.assertEquals("Service.WRONG_CREDENTIALS", exception.getMessage());
